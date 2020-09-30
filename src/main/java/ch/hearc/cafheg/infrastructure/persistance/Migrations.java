40e85dcea42f65a58cc94378e26132017a6c03f0
Arnaud Geiser <arnaudgeiser@gmail.com>
2020-09-30 07:54:28 +0200

    Introduces a seam to help students to avoid the DML operations in case of tests

--- src/main/java/ch/hearc/cafheg/infrastructure/persistance/Migrations.java
+++ src/main/java/ch/hearc/cafheg/infrastructure/persistance/Migrations.java
@@ -1,19 +1,33 @@
 package ch.hearc.cafheg.infrastructure.persistance;
 
-import javax.sql.DataSource;
+import java.util.List;
 import org.flywaydb.core.Flyway;
 
 public class Migrations {
 
   private final Database database;
+  private final boolean forTest;
 
   public Migrations(Database database) {
     this.database = database;
+    this.forTest = false;
   }
 
   public void start() {
     System.out.println("Doing migrations");
-    Flyway flyway = Flyway.configure().dataSource(database.getDataSource()).locations("classpath:db").load();
+
+    String location;
+    if(forTest) {
+      location =  "classpath:db/ddl";
+    } else {
+      location =  "classpath:db";
+    }
+
+    Flyway flyway = Flyway.configure()
+        .dataSource(database.getDataSource())
+        .locations(location)
+        .load();
+
     flyway.migrate();
     System.out.println("Migrations done");
   }

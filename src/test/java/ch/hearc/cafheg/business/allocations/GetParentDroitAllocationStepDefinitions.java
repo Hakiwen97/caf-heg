4cf6c17cd000eacc6260ec5d910252aedf9a9b5e
Hakiwen97 <adriensigrist@hotmail.com>
2020-11-27 10:06:39 +0100

    Ajout des définitions

---
+++ src/test/java/ch/hearc/cafheg/business/allocations/GetParentDroitAllocationStepDefinitions.java
@@ -0,0 +1,7 @@
+package ch.hearc.cafheg.business.allocations;
+
+public class GetParentDroitAllocationStepDefinitions {
+    @io.cucumber.java.en.And("^the other has not$")
+    public void theOtherHasNot() {
+    }
+}

2ff4c901eeb1fd406696d96a5c21b59c3d61907a
Hakiwen97 <adriensigrist@hotmail.com>
2020-12-14 10:16:15 +0100

    Ajout des plugins pour les tests d'intégration

---
+++ src/integration-test/java/ch/hearc/cafheg/integration/MyTestsIT.java
@@ -0,0 +1,18 @@
+package ch.hearc.cafheg.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyTestsIT {
+    @Test
+    void testSimple_Given1_ShouldBe1(){
+        assertTrue(1==1);
+    }
+
+
+
+
+
+    
+}
